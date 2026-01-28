80a42e393110e78062ef2a94878418784a4a7f86
SamHuang <sam821203@yahoo.com.tw>
2026-01-28 22:47:02 +0800

    chore(eslint): add ESLint rules and project conventions

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,170 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import { FlatCompat } from '@eslint/eslintrc'
+import { defineConfig } from 'eslint/config'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+  ...compat.config({
+    root: true,
+    env: {
+      node: true,
+      es6: true,
     },
-  },
+    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    ignorePatterns: [
+      'dist/*',
+      'node_modules/*',
+      'public/mockServiceWorker.js',
+      'generators/*',
+    ],
+    extends: ['eslint:recommended'],
+    plugins: ['check-file'],
+    overrides: [
+      {
+        files: ['**/*.ts', '**/*.tsx'],
+        parser: '@typescript-eslint/parser',
+        settings: {
+          react: { version: 'detect' },
+          'import/resolver': {
+            typescript: {},
+          },
+        },
+        env: {
+          browser: true,
+          node: true,
+          es6: true,
+        },
+        extends: [
+          'eslint:recommended',
+          'plugin:import/errors',
+          'plugin:import/warnings',
+          'plugin:import/typescript',
+          'plugin:@typescript-eslint/recommended',
+          'plugin:react/recommended',
+          'plugin:react-hooks/recommended',
+          'plugin:jsx-a11y/recommended',
+          'plugin:prettier/recommended',
+          'plugin:testing-library/react',
+          'plugin:jest-dom/recommended',
+          'plugin:tailwindcss/recommended',
+          'plugin:vitest/legacy-recommended',
+        ],
+        rules: {
+          'import/no-restricted-paths': [
+            'error',
+            {
+              zones: [
+                // disables cross-feature imports:
+                // eg. src/features/discussions should not import from src/features/comments, etc.
+                {
+                  target: './src/features/auth',
+                  from: './src/features',
+                  except: ['./auth'],
+                },
+                {
+                  target: './src/features/comments',
+                  from: './src/features',
+                  except: ['./comments'],
+                },
+                {
+                  target: './src/features/discussions',
+                  from: './src/features',
+                  except: ['./discussions'],
+                },
+                {
+                  target: './src/features/teams',
+                  from: './src/features',
+                  except: ['./teams'],
+                },
+                {
+                  target: './src/features/users',
+                  from: './src/features',
+                  except: ['./users'],
+                },
+                // enforce unidirectional codebase:
+
+                // e.g. src/app can import from src/features but not the other way around
+                {
+                  target: './src/features',
+                  from: './src/app',
+                },
+
+                // e.g src/features and src/app can import from these shared modules but not the other way around
+                {
+                  target: [
+                    './src/components',
+                    './src/hooks',
+                    './src/lib',
+                    './src/types',
+                    './src/utils',
+                  ],
+                  from: ['./src/features', './src/app'],
+                },
+              ],
+            },
+          ],
+          'import/no-cycle': 'error',
+          'linebreak-style': ['error', 'unix'],
+          'react/prop-types': 'off',
+          'import/order': [
+            'error',
+            {
+              groups: [
+                'builtin',
+                'external',
+                'internal',
+                'parent',
+                'sibling',
+                'index',
+                'object',
+              ],
+              'newlines-between': 'always',
+              alphabetize: { order: 'asc', caseInsensitive: true },
+            },
+          ],
+          'import/default': 'off',
+          'import/no-named-as-default-member': 'off',
+          'import/no-named-as-default': 'off',
+          'react/react-in-jsx-scope': 'off',
+          'jsx-a11y/anchor-is-valid': 'off',
+          '@typescript-eslint/no-unused-vars': ['error'],
+          '@typescript-eslint/explicit-function-return-type': ['off'],
+          '@typescript-eslint/explicit-module-boundary-types': ['off'],
+          '@typescript-eslint/no-empty-function': ['off'],
+          '@typescript-eslint/no-explicit-any': ['off'],
+          'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+          'check-file/filename-naming-convention': [
+            'error',
+            {
+              '**/*.{ts,tsx}': 'KEBAB_CASE',
+            },
+            {
+              ignoreMiddleExtensions: true,
+            },
+          ],
+        },
+      },
+      {
+        plugins: ['check-file'],
+        files: ['src/**'],
+        excludedFiles: ['src/**/__tests__/**'],
+        rules: {
+          'check-file/folder-naming-convention': [
+            'error',
+            {
+              '**/*': 'KEBAB_CASE',
+            },
+          ],
+        },
+      },
+    ],
+  }),
 ])
